bf25c6f98dd9f079c2480d8e0acfd875a434cdb3
544sourav <kumarsourav250882@gmail.com>
2024-10-19 11:57:01 +0530

    updated

--- src/data/homeinfo.jsx
+++ src/data/homeinfo.jsx
@@ -1,27 +1,27 @@
 export const HomeInfoData = [
-    {
-      "id": 1,
-      "desc": `Hi, 👋 I am Sourav Sharma and I am a developer.`,
-      "btnText": "Download Resume",
-       "path":"https://drive.google.com/file/d/1XZTGg-sl1djLXTQS-PfCSAUV8843Hesc/view?usp=sharing"
-    },
-    {
-      "id": 2,
-      "desc": "Learn more about my background and skills.",
-      "btnText": "About Me",
-      "path": "/about"
-    },
-    {
-      "id": 3,
-      "desc": "Explore my latest projects.",
-      "btnText": "View Projects",
-      "path": "/projects"
-    },
-    {
-      "id": 4,
-      "desc": "Reach out to me for collaborations or inquiries.",
-      "btnText": "let's talk",
-      "path": "/contact"
-    }
-  ]
+  {
+    id: 1,
+    desc: `Hi, 👋 I am Sourav Sharma and I am a developer.`,
+    btnText: "Download Resume",
+    path: "https://drive.google.com/file/d/1vMTcFYmtpues054txeHo4CZIWJyqRqIR/view?usp=sharing",
+  },
+  {
+    id: 2,
+    desc: "Learn more about my background and skills.",
+    btnText: "About Me",
+    path: "/about",
+  },
+  {
+    id: 3,
+    desc: "Explore my latest projects.",
+    btnText: "View Projects",
+    path: "/projects",
+  },
+  {
+    id: 4,
+    desc: "Reach out to me for collaborations or inquiries.",
+    btnText: "let's talk",
+    path: "/contact",
+  },
+];
 
--- src/data/index.js
+++ src/data/index.js
@@ -28,91 +28,98 @@ import {
     figma,
     cplus,
     clang,
-    postman
+    socketio,
+    postman,
+    
 } from "../assets/icons";
 
 export const skills = [
-    {
-        imageUrl: css,
-        name: "CSS",
-        type: "Frontend",
-    },
-    {
-        imageUrl: express,
-        name: "Express",
-        type: "Backend",
-    },
-    {
-        imageUrl: git,
-        name: "Git",
-        type: "Version Control",
-    },
-    {
-        imageUrl: github,
-        name: "GitHub",
-        type: "Version Control",
-    },
-    {
-        imageUrl: html,
-        name: "HTML",
-        type: "Frontend",
-    },
-    {
-        imageUrl: javascript,
-        name: "JavaScript",
-        type: "Frontend",
-    },
-    {
-        imageUrl: mongodb,
-        name: "MongoDB",
-        type: "Database",
-    },
-    {
-        imageUrl: photoshop,
-        name: "Photoshop",
-        type: "design",
-    },
-    {
-        imageUrl: figma,
-        name: "Figma",
-        type: "design",
-    },
-    {
-        imageUrl: cplus,
-        name: "C++",
-        type: "Programing",
-    },
-    {
-        imageUrl: clang,
-        name: "C language",
-        type: "Programing",
-    },
-    {
-        imageUrl: nodejs,
-        name: "Node.js",
-        type: "Backend",
-    },
-    {
-        imageUrl: react,
-        name: "React",
-        type: "Frontend",
-    },
-    {
-        imageUrl: redux,
-        name: "Redux",
-        type: "State Management",
-    },
-   
-    {
-        imageUrl: tailwindcss,
-        name: "Tailwind CSS",
-        type: "Frontend",
-    },
-    {
-        imageUrl: postman,
-        name: "Postman",
-        type: "Backend",
-    }
+  {
+    imageUrl: css,
+    name: "CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: express,
+    name: "Express",
+    type: "Backend",
+  },
+  {
+    imageUrl: git,
+    name: "Git",
+    type: "Version Control",
+  },
+  {
+    imageUrl: github,
+    name: "GitHub",
+    type: "Version Control",
+  },
+  {
+    imageUrl: html,
+    name: "HTML",
+    type: "Frontend",
+  },
+  {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: mongodb,
+    name: "MongoDB",
+    type: "Database",
+  },
+  {
+    imageUrl: photoshop,
+    name: "Photoshop",
+    type: "design",
+  },
+  {
+    imageUrl: figma,
+    name: "Figma",
+    type: "design",
+  },
+  {
+    imageUrl: cplus,
+    name: "C++",
+    type: "Programing",
+  },
+  {
+    imageUrl: clang,
+    name: "C language",
+    type: "Programing",
+  },
+  {
+    imageUrl: nodejs,
+    name: "Node.js",
+    type: "Backend",
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend",
+  },
+  {
+    imageUrl: redux,
+    name: "Redux",
+    type: "State Management",
+  },
+
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: socketio,
+    name: "Socket.IO",
+    type: "Web-socket",
+  },
+  {
+    imageUrl: postman,
+    name: "Postman",
+    type: "Backend",
+  },
 ];
 
 export const socialLinks = [
